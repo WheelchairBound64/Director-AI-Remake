924217ea17110d24cf05ef54c9379f4159c5f690
WheelchairBound64 <123192532+WheelchairBound64@users.noreply.github.com>
2024-11-15 14:12:46 -0600

    Test

--- Assets/Scripts/Cope.cs
+++ Assets/Scripts/Cope.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+public class Cope : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        //seethe
     }
 
     // Update is called once per frame
--- Assets/Scripts/Player 2.cs
+++ Assets/Scripts/Player 2.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+// comment test
 
 public class Player : MonoBehaviour
 {
